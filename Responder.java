ef80dbd082e6d2a63498c280ebf490d786c2c7b8
ctman96 <ctman96@students.cs.ubc.ca>
2020-01-22 15:22:17 -0800

    Initial Responder

--- Responder.java
+++ Responder.java
@@ -1,11 +1,69 @@
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.net.DatagramPacket;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 // This class sets up and controls the part of the system that responds to heartbeat messages
 
 public class Responder {
+    private static Dictionary<String, Boolean> addrPairs = new Hashtable<>();
+    private int port;
+    private InetAddress laddr;
+    private DatagramSocket socket;
+
+    private Thread thread;
+    private ResponderHandler handler;
+
+    // Runnable handler class to respond to packets
+    private static class ResponderHandler implements Runnable {
+        private boolean responding = false;
+        private DatagramSocket socket;
+
+        boolean isResponding() {
+            return responding;
+        }
+
+         void setResponding(boolean responding) {
+            this.responding = responding;
+        }
+
+        ResponderHandler(DatagramSocket socket) {
+            this.socket = socket;
+        }
+
+        public void run() {
+            while(true) {
+                try {
+                    // Receive packet
+                    byte[] buf = new byte[16]; // TODO: right size?
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                    socket.receive(packet);
+
+                    // Process HeartBeat
+                    InetAddress address = packet.getAddress();
+                    int port = packet.getPort();
+
+                    ByteBuffer bb = ByteBuffer.wrap(buf);
+                    long epochNonce = bb.getLong();
+                    long seqNum = bb.getLong();
+
+                    System.out.println(epochNonce + ", " + seqNum); // TODO debugging - remove
+
+                    // Reply Ack
+                    DatagramPacket ack = new DatagramPacket(buf, buf.length, address, port);
+                    socket.send(ack);
+                } catch (IOException ex) {
+                    // TODO catch here or what?
+                }
+            }
+        }
+    }
 
     // This constructor creates a Responder object.
     //    port -- the port to listen for heartbeat messages
@@ -18,7 +76,27 @@ public class Responder {
 
 
     public Responder(int port, InetAddress laddr) throws FailureDetectorException, SocketException {
-	System.out.println("Responder needs to be implemented");
+        // TODO: test if this is actually necessary to track myself?
+        this.port = port;
+        this.laddr = laddr;
+        String pair = port + laddr.toString();
+        try {
+            if (Responder.addrPairs.get(pair).equals(Boolean.TRUE)) {
+                throw new SocketException();
+            }
+        } catch (NullPointerException ex) {
+            // Expected
+        }
+        try {
+            socket = new DatagramSocket(port, laddr);
+        } catch (SecurityException ex) {
+            throw new FailureDetectorException("Security Exception"); // TODO?
+        }
+        handler = new ResponderHandler(socket); // TODO: here or in startResponding?
+        thread = new Thread(handler);
+        thread.setDaemon(true);
+        thread.start();
+        Responder.addrPairs.put(pair, Boolean.TRUE);
     }
 
     // Prior to this method being invoked all heartbeat messages are ignored/discarded.
@@ -26,14 +104,17 @@ public class Responder {
     // a FailureDectectorException with the message "Already Running" is to be thrown.
     // If any other problem is detected then the FailureDectorException is thrown
     public void startResponding()throws FailureDetectorException {
-	System.out.println("startResponding needs to be implemented");
+        if (!handler.isResponding()) {
+            handler.setResponding(true);
+        } else {
+            throw new FailureDetectorException("Already Running");
+        }
     }
 
     // Once this method returns heartbeat messages will be discraded/ignored until a subsequent
     // startResponding call is made.
-
     public void stopResponding() {
-	System.out.println("stopResponding needs to be implemented");
+        handler.setResponding(false);
     }
 }
     
